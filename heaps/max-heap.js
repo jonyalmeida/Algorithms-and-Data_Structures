171cc18d58bc029d8c67f5dd32b57e54c8445d1f
jon <jon@jons-MacBook-Pro.local>
2020-12-13 19:05:49 -0600

    add heap methods sift up sift down

--- heaps/max-heap.js
+++ heaps/max-heap.js
@@ -15,14 +15,51 @@ class MaxHeap {
         //base case, item already at top, return
         if (idx === 1) return;
 
-        let parentIdx = Math.floor(idx / 2);
+        //get parent's index
+        const parentIdx = Math.floor(idx / 2);
+
+        //swap if parent is less than item
         if (this.array[idx] > this.array[parentIdx]) {
             [this.array[idx], this.array[parentIdx]] = [
                 this.array[parentIdx],
                 this.array[idx],
             ];
+            //recursive call
             this.siftUp(parentIdx);
         }
+    }
+
+    deleteMax() {
+        let max = this.array[1];
+        this.array[1] = this.array.pop();
+
+        this.siftDown(1);
+
+        return max;
+    }
+
+    siftDown(idx) {
+        let leftIdx = idx * 2;
+        let rightIdx = idx * 2 + 1;
+        let ary = this.array;
+        let leftVal = ary[leftIdx];
+        let rightVal = ary[rightIdx];
+        let val = ary[idx];
+
+        if (leftVal === undefined) leftVal = -Infinity;
+        if (rightVal === undefined) rightVal = -Infinity;
+
+        if (val > leftVal && val > rightVal) return;
+
+        let swapIdx;
+        if (leftVal > rightVal) {
+            swapIdx = leftIdx;
+        } else {
+            swapIdx = rightIdx;
+        }
+        [ary[swapIdx], ary[idx]] = [ary[idx], ary[swapIdx]];
+
+        this.siftDown(swapIdx);
     }
 }
 
@@ -35,5 +72,10 @@ heap.insert(12);
 heap.insert(9);
 heap.insert(53);
 heap.insert(101);
+heap.insert(1);
 
 console.log(heap);
+console.log(heap.deleteMax());
+console.log(heap.deleteMax());
+console.log(heap);
+console.log(heap.deleteMax());
